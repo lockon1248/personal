14582afa03e200e3d58e588b8f564ff9da1b1ca7
lockon1248 <tom5678tom5678t@yahoo.com.tw>
2023-06-12 11:50:42 +0800

    約拍修改

--- js/contact_us.js
+++ js/contact_us.js
@@ -9,12 +9,6 @@ owl.owlCarousel({
     autoplayHoverPause: false,
 
 });
-$('.play').on('click', function () {
-    owl.trigger('play.owl.autoplay', [2000])
-})
-$('.stop').on('click', function () {
-    owl.trigger('stop.owl.autoplay')
-}) 
 })
 
 
